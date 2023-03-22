75dfd1c74c0764b2441c5d4231296494150bd76a
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-22 16:19:21 +0100

    FEAT initial method for axios call

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            // qui aggiungo nei data il mio todo, che inizialmente e' vuoto ma poi gli pusho i file della chimata axios 
+            todos: [],
         }
+    },
+    methods: {
+        // qui creo una funzione per andarmi a prendere i file tramite una chimata axios al mio API/server.php
+        searchItems() {
+            axios.get('./todo.php')
+        }
+    },
+    mounted() {
+        // faccio questa chiamata appena viene montata la mia pagina perche' voglio recuperare i dati dal mio server tutte le volte che aggiorno la pagina 
+        this.searchItems()
     }
 }).mount('#app')
