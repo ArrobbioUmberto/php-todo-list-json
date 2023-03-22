9f374378438733106211201cecc31cf48e099895
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-22 19:18:39 +0100

    FEAT logic for add

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-
-
 const { createApp } = Vue
 
 createApp({
     data() {
         return {
-            message: 'Hello Vue!',
+            message: 'To Do List',
             // qui aggiungo nei data il mio todo, che inizialmente e' vuoto ma poi gli pusho i file della chimata axios 
             todos: [],
+            newTodo: '',
+            completeTasks: [
+                {
+                    text: '',
+                    done: true,
+                }
+            ]
         }
     },
     methods: {
         // qui creo una funzione per andarmi a prendere i file tramite una chimata axios al mio API/server.php
         searchItems() {
             axios.get('./todo.php').then(res => {
-                console.log(res.data, res.data[0])
+                console.log(res.data)
                 this.todos = res.data
             })
                 .catch((err) => {
@@ -22,6 +27,20 @@ createApp({
                     this.todos = []
                 })
         },
+        addItem() {
+            console.log('save task', this.newTodo)
+            $data = {
+                todo: this.newTodo,
+            }
+            axios.post('./todo.php', $data, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then(res => {
+                this.todos = res.data;
+                this.newTodo = ''
+            })
+        }
     },
     mounted() {
         // faccio questa chiamata appena viene montata la mia pagina perche' voglio recuperare i dati dal mio server tutte le volte che aggiorno la pagina 
