f267afa413b0dac165351dc6427eba251706f18c
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-22 19:19:24 +0100

    FEAT add new task but bug with old json file

--- index.php
+++ index.php
@@ -15,13 +15,19 @@
 <body>
 
     <div id="app">
-        <div class="container">
+        <div class="container py-5">
             <h1>{{ message }}</h1>
         </div>
         <div class="container">
-            <ul class="list-group">
-                <li v-for="(item,index) in todos" :key="item" class="list-group-item">{{item.text}}</li>
-            </ul>
+            <div class="row">
+                <div class="col">
+                    <input @keyup.enter="addItem" v-model="newTodo" type="text" placeholder="Scrivi nella lista " class="mb-3 form-control" name="todo" aria-label="default input example">
+                    <ul class="list-group">
+                        <li v-for="(item,index) in todos" :key="item" class="list-group-item">{{item}}</li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     </div>
     <script src="./app.js"></script>
