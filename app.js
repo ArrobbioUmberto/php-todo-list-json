e3414b9aee28ec7660ece94d9119a7b3c0449cfc
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-22 17:38:02 +0100

    FEAT axios call for data

--- app.js
+++ app.js
@@ -13,8 +13,15 @@ createApp({
     methods: {
         // qui creo una funzione per andarmi a prendere i file tramite una chimata axios al mio API/server.php
         searchItems() {
-            axios.get('./todo.php')
-        }
+            axios.get('./todo.php').then(res => {
+                console.log(res.data, res.data[0])
+                this.todos = res.data
+            })
+                .catch((err) => {
+                    console.log(err)
+                    this.todos = []
+                })
+        },
     },
     mounted() {
         // faccio questa chiamata appena viene montata la mia pagina perche' voglio recuperare i dati dal mio server tutte le volte che aggiorno la pagina 
