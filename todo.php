558ed9af8a577d30f6ab7dad7b39ee687496d15a
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-22 16:19:34 +0100

    FEAT comments

--- todo.php
+++ todo.php
@@ -4,7 +4,7 @@
 $string = file_get_contents('./todo.json');
 
 $todo = json_decode($string, true);
-
+// imposto l'header 
 header('Content-Type: application/json');
 // in questo modo possiamo modicare un array in un file json 
 echo json_encode($todo);
