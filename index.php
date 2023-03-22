bb3673fef0c5591d1004e6f6dbac6738ded99d14
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-03-22 15:39:12 +0100

    FEAT convert array in file json

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+
+$todo = ['HTML', 'CSS', 'Responsive design', 'Javascript', 'PHP', 'Laravel'];
+
+header('Content-Type: application/json');
+echo json_encode(($todo));
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
